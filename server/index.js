d07792a10b9c9d3239a342e8c854d595807bbe0d
Tomas Fisher <dontomassino777@gmail.com>
2022-01-07 15:04:22 -0700

    controllers added

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3001;
+app.listen(3001, () => {
+    console.log(`Jammin on ${port}`);
+});
