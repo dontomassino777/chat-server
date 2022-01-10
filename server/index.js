30f9e31d07e9d0b367bc87b67e5af584f85625de
Tomas Fisher <dontomassino777@gmail.com>
2022-01-10 11:20:19 -0700

    project complete

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const mc = require('./controllers/messages_controller');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
 const messagesBaseUrl = "/api/messages";
-
 app.post(messagesBaseUrl, mc.create);
 app.get(messagesBaseUrl, mc. read);
 app.put(`${messagesBaseUrl}/:id`, mc.update);
